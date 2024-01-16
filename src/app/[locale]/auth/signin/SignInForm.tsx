aed7513f49fb3fe115a16386ac6129885eeac12b
kenyontu <kenyontu@outlook.com>
2024-01-16 12:53:01 +0800

    use plain form validation for sign in

--- src/app/[locale]/auth/signin/SignInForm.tsx
+++ src/app/[locale]/auth/signin/SignInForm.tsx
@@ -4,7 +4,6 @@ import { signIn } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
 import { AuthInput } from '~/components/auth/AuthInput'
 import { Button } from '~/components/Button'
 
@@ -12,11 +11,7 @@ import styles from '~/styles/Auth.module.scss'
 import { Card } from '~/components/Card'
 import { Typography } from '~/components/Typography'
 import { Link } from '~/navigation'
-
-type Input = {
-  email: string
-  password: string
-}
+import { useRouter } from 'next/navigation'
 
 type Props = {
   t: {
@@ -34,34 +29,29 @@ type Props = {
 export function SignInForm({ t }: Props) {
   const [isLoading, setIsLoading] = useState(false)
   const [authError, setAuthError] = useState<string | null>(null)
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    setFocus,
-  } = useForm<Input>()
-  const onSubmit: SubmitHandler<Input> = async ({ email, password }) => {
+  const router = useRouter()
+
+  const formAction = async (formData: FormData) => {
     setAuthError(null)
     setIsLoading(true)
 
     try {
       const res = await signIn('credentials', {
-        email,
-        password,
+        email: formData.get('email'),
+        password: formData.get('password'),
         redirect: false,
       })
 
-      if (!res || !res.ok) {
+      if (res?.ok) {
+        router.replace('/')
+      } else {
         setIsLoading(false)
-        setFocus('email')
-        reset()
         setAuthError(t.invalidEmailPassword)
       }
     } catch (error) {
       // TODO: Report error
-      console.error(error)
-      throw new Error(t.requestError)
+      setIsLoading(false)
+      setAuthError(t.requestError)
     }
   }
 
@@ -87,27 +77,21 @@ export function SignInForm({ t }: Props) {
             {t.title}
           </Typography>
 
-          <form
-            method="post"
-            className={styles.form}
-            onSubmit={handleSubmit(onSubmit)}
-          >
+          <form action={formAction} className={styles.form}>
             <AuthInput
+              name="email"
               type="email"
               placeholder={t.emailAddress}
               aria-label={t.emailAddress}
               autoFocus
-              {...register('email', { required: true })}
-              aria-invalid={errors.email ? 'true' : 'false'}
-              error={errors.email?.message}
+              required
             />
             <AuthInput
+              name="password"
               type="password"
               placeholder={t.password}
               aria-label={t.password}
-              {...register('password', { required: true })}
-              aria-invalid={errors.password ? 'true' : 'false'}
-              error={errors.password?.message}
+              required
             />
 
             {authError && (
