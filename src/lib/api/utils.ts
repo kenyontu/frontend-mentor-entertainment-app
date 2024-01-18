fb1ff6da9a62e44f45c396949d61612745f1b0eb
kenyontu <kenyontu@outlook.com>
2024-01-18 10:11:33 +0800

    remove prisma
    It has been replaced by Kysely

--- src/lib/api/utils.ts
+++
@@ -1,6 +0,0 @@
-import { z, ZodTypeAny } from 'zod'
-
-export type FormErrorResponse<T extends ZodTypeAny> = {
-  message?: string
-  form?: z.inferFlattenedErrors<T>
-}
--- src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import postgres from 'postgres'
-
-export const sql = postgres(
-  'postgres://appuser:pass@localhost:5432/entertainment_app'
-)
