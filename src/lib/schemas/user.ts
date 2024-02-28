ed96ea2c2c4cd0c7c99ae093b53ef052dbb925a0
kenyontu <kenyontu@outlook.com>
2024-02-28 13:10:31 +0800

    remove unused schema

--- src/lib/schemas/user.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod'
-
-export const userSchema = z.object({
-  name: z.string().min(1, 'Name cannot be empty'),
-  email: z.string().email('Not a valid email'),
-  password: z.string().min(5, 'Must have a minimum of 5 characters'),
-})
