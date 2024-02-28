434527bf2afbfa4bbc0ec27bc29dd00a23d9f174
kenyontu <kenyontu@outlook.com>
2024-02-28 13:36:54 +0800

    clean up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,4 @@
-export function assert(
-  condition: boolean,
-  message: string,
-): asserts condition {
+export function assert(condition: boolean, message: string): asserts condition {
   if (!condition) {
     throw new Error(message)
   }
@@ -11,19 +8,4 @@ export function getSingleQueryValue(query?: string[] | string) {
   const result = query && (Array.isArray(query) ? query[0] : query)
 
   return typeof result === 'string' ? result : undefined
-}
-
-export function getSearchResultText(
-  length: number,
-  searchTerm: string,
-) {
-  if (length > 1) {
-    return `Found ${length} results for '${searchTerm}'`
-  }
-
-  if (length === 1) {
-    return `Found 1 result for '${searchTerm}'`
-  }
-
-  return `No result for '${searchTerm}'`
 }
